808eda12f695c9d6249a8213863776c28746af45
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-05-22 02:35:14 +0300

    Finished the javascript code

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const topLinks = document.querySelector(".top-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  topLinks.classList.toggle("active");
+  console.log('sdsd')
+})
