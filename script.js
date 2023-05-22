f3a7e3bb4af5fbb9e271e6c7af3ac43724fd58ae
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-05-22 14:43:46 +0300

    More css adjustments to top links

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const initApp = () => {
-  const hamburger = document.querySelector(".hamburger");
-  const topLinks = document.querySelector(".top-links");
+  const hamburger = document.querySelector('.hamburger');
+  const topLinks = document.querySelector('.top-links');
 
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    topLinks.classList.toggle("active");
-    console.log('sdsd');
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    topLinks.classList.toggle('active');
   });
-    document.querySelectorAll('.top-link').forEach(n => n.addEventListener('click', () => {
+
+  document.querySelectorAll('.top-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     topLinks.classList.remove('active');
-  }))
+  }));
 };
 
 document.addEventListener('DOMContentLoaded', () => {
