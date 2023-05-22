9f633356fafa3d21a2bf8d7a30f02402c206dca0
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-05-23 02:15:04 +0300

    finished the preview but not working

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ const initApp = () => {
     hamburger.classList.remove('active');
     topLinks.classList.remove('active');
   }));
+  let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.card .view').forEach(view =>{
+  view.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = view.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
 };
 
 document.addEventListener('DOMContentLoaded', () => {
