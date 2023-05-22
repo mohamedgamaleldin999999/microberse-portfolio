3335a26cfcfc2fe3626cba3ad0b4480a3e4f310c
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-05-22 13:53:27 +0300

    Add DOM loading so the js code could work

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const topLinks = document.querySelector(".top-links");
+const initApp = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const topLinks = document.querySelector(".top-links");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  topLinks.classList.toggle("active");
-  console.log('sdsd');
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    topLinks.classList.toggle("active");
+    console.log('sdsd');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  initApp();
 });
