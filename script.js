0cb9a1b0b5d2437aa5991ff5735e35af9365b339
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-05-26 06:33:10 +0300

    Added form validation to lower case

--- script.js
+++ script.js
@@ -183,6 +183,26 @@ const initApp = () => {
       overlay.style.display = 'block';
     });
   });
+
+  const form = document.querySelector('.form');
+  const email = document.querySelector('#mail');
+  const errMsg = document.querySelector('.error-massage');
+  const msgText = 'please use lowercase letters only';
+
+  form.addEventListener('submit', (e) => {
+    if (email.value !== email.value.toLowerCase()) {
+      errMsg.textContent = msgText;
+      errMsg.style.display = 'flex';
+      errMsg.style.color = 'red';
+      errMsg.style.fontSize = '13px';
+      errMsg.style.marginLeft = '20px';
+      email.style.border = '1px solid red';
+      e.preventDefault();
+    } else {
+      errMsg.style.display = 'none';
+      email.style.border = '1px solid green';
+    }
+});
 };
 
 document.addEventListener('DOMContentLoaded', () => {
