4017a9e9ed7eadb16f618d3b14c0158f03fa17b8
Umu <hanny.ally123@gmail.com>
2023-05-26 23:49:00 +0300

    Add preservation data function..

--- script.js
+++ script.js
@@ -1,106 +1,126 @@
 const initApp = () => {
-  const hamburger = document.querySelector('.hamburger');
-  const topLinks = document.querySelector('.top-links');
+  const hamburger = document.querySelector(".hamburger");
+  const topLinks = document.querySelector(".top-links");
 
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    topLinks.classList.toggle('active');
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    topLinks.classList.toggle("active");
   });
 
-  document.querySelectorAll('.top-link').forEach((n) => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    topLinks.classList.remove('active');
-  }));
+  document.querySelectorAll(".top-link").forEach((n) =>
+    n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      topLinks.classList.remove("active");
+    })
+  );
 
   const projects = [
     {
       name: "Most-Post Storie's <br/> Gain+Glory",
-      description: 'Project 1 description',
-      image: { mobile: './images/Snapshoot Portfolio (1).png', desktop: './images/desk-porto.png' },
-      technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-      liveLink: 'https://example.com/live1',
-      sourceLink: 'https://example.com/source1',
+      description: "Project 1 description",
+      image: {
+        mobile: "./images/Snapshoot Portfolio (1).png",
+        desktop: "./images/desk-porto.png",
+      },
+      technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+      liveLink: "https://example.com/live1",
+      sourceLink: "https://example.com/source1",
     },
 
     {
       name: "Most-Post Storie's <br/> Gain+Glory",
-      description: 'Project 2 description',
-      image: { mobile: './images/Snapshoot Portfolio (1).png', desktop: './images/desk-porto.png' },
-      technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-      liveLink: 'https://example.com/live2',
-      sourceLink: 'https://example.com/source2',
+      description: "Project 2 description",
+      image: {
+        mobile: "./images/Snapshoot Portfolio (1).png",
+        desktop: "./images/desk-porto.png",
+      },
+      technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+      liveLink: "https://example.com/live2",
+      sourceLink: "https://example.com/source2",
     },
 
     {
       name: "Most-Post Storie's <br/> Gain+Glory",
-      description: 'Project 3 description',
-      image: { mobile: './images/Snapshoot Portfolio (1).png', desktop: './images/desk-porto.png' },
-      technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-      liveLink: 'https://example.com/live2',
-      sourceLink: 'https://example.com/source2',
+      description: "Project 3 description",
+      image: {
+        mobile: "./images/Snapshoot Portfolio (1).png",
+        desktop: "./images/desk-porto.png",
+      },
+      technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+      liveLink: "https://example.com/live2",
+      sourceLink: "https://example.com/source2",
     },
 
     {
       name: "Most-Post Storie's <br/> Gain+Glory",
-      description: 'Project 4 description',
-      image: { mobile: './images/Snapshoot Portfolio (1).png', desktop: './images/desk-porto.png' },
-      technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-      liveLink: 'https://example.com/live2',
-      sourceLink: 'https://example.com/source2',
+      description: "Project 4 description",
+      image: {
+        mobile: "./images/Snapshoot Portfolio (1).png",
+        desktop: "./images/desk-porto.png",
+      },
+      technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+      liveLink: "https://example.com/live2",
+      sourceLink: "https://example.com/source2",
     },
 
     {
       name: "Most-Post Storie's <br/> Gain+Glory",
-      description: 'Project 5 description',
-      image: { mobile: './images/Snapshoot Portfolio (1).png', desktop: './images/desk-porto.png' },
-      technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-      liveLink: 'https://example.com/live2',
-      sourceLink: 'https://example.com/source2',
+      description: "Project 5 description",
+      image: {
+        mobile: "./images/Snapshoot Portfolio (1).png",
+        desktop: "./images/desk-porto.png",
+      },
+      technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+      liveLink: "https://example.com/live2",
+      sourceLink: "https://example.com/source2",
     },
 
     {
       name: "Most-Post Storie's <br/> Gain+Glory",
-      description: 'Project 6 description',
-      image: { mobile: './images/Snapshoot Portfolio (1).png', desktop: './images/desk-porto.png' },
-      technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-      liveLink: 'https://example.com/live2',
-      sourceLink: 'https://example.com/source2',
+      description: "Project 6 description",
+      image: {
+        mobile: "./images/Snapshoot Portfolio (1).png",
+        desktop: "./images/desk-porto.png",
+      },
+      technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+      liveLink: "https://example.com/live2",
+      sourceLink: "https://example.com/source2",
     },
   ];
 
-  const projectsContainer = document.querySelector('.cards');
+  const projectsContainer = document.querySelector(".cards");
 
   projects.forEach((project) => {
-    const projectElement = document.createElement('div');
-    projectElement.classList.add('card');
+    const projectElement = document.createElement("div");
+    projectElement.classList.add("card");
 
-    const imageElement = document.createElement('div');
-    imageElement.classList.add('card-img');
+    const imageElement = document.createElement("div");
+    imageElement.classList.add("card-img");
     projectElement.appendChild(imageElement);
 
-    const contentElement = document.createElement('div');
-    contentElement.classList.add('info');
+    const contentElement = document.createElement("div");
+    contentElement.classList.add("info");
     projectElement.appendChild(contentElement);
 
-    const titleElement = document.createElement('h3');
-    titleElement.classList.add('project-title');
+    const titleElement = document.createElement("h3");
+    titleElement.classList.add("project-title");
     titleElement.innerHTML = project.name;
     contentElement.appendChild(titleElement);
 
-    const langListElement = document.createElement('ul');
-    langListElement.classList.add('languages');
+    const langListElement = document.createElement("ul");
+    langListElement.classList.add("languages");
     contentElement.appendChild(langListElement);
 
     project.technologies.forEach((technology) => {
-      const langItemElement = document.createElement('li');
-      langItemElement.classList.add('language');
+      const langItemElement = document.createElement("li");
+      langItemElement.classList.add("language");
       langItemElement.textContent = technology;
       langListElement.appendChild(langItemElement);
     });
     // dynamic modal
     const displayPopup = (project) => {
-      const dynamicSection = document.getElementById('dynamic-section');
-      const list = '';
+      const dynamicSection = document.getElementById("dynamic-section");
+      const list = "";
       project.technologies.map((technology) => `<li>${technology}</li>`);
       const sectioHTML = `
 <section id='model-section' class='model-section'>
@@ -157,34 +177,40 @@ const initApp = () => {
     `;
       dynamicSection.innerHTML = sectioHTML;
 
-      const overlay = document.getElementById('overlay');
-      const modelSection = document.querySelector('.model');
-      const modelCloseButton = document.querySelector('.model-close');
-      modelCloseButton.addEventListener('click', () => {
-        modelSection.style.display = 'none';
-        overlay.style.display = 'none';
+      const overlay = document.getElementById("overlay");
+      const modelSection = document.querySelector(".model");
+      const modelCloseButton = document.querySelector(".model-close");
+      modelCloseButton.addEventListener("click", () => {
+        modelSection.style.display = "none";
+        overlay.style.display = "none";
       });
     };
 
     //  'See Project' button
-    const buttonElement = document.createElement('button');
-    buttonElement.classList.add('view-btn');
-    buttonElement.textContent = 'See Project';
+    const buttonElement = document.createElement("button");
+    buttonElement.classList.add("view-btn");
+    buttonElement.textContent = "See Project";
     contentElement.appendChild(buttonElement);
 
     // Append for projects container
     projectsContainer.appendChild(projectElement);
-    buttonElement.addEventListener('click', () => {
+    buttonElement.addEventListener("click", () => {
       displayPopup(project);
 
-      const modelSection = document.querySelector('.model');
-      const overlay = document.getElementById('overlay');
-      modelSection.style.display = 'block';
-      overlay.style.display = 'block';
+      const modelSection = document.querySelector(".model");
+      const overlay = document.getElementById("overlay");
+      modelSection.style.display = "block";
+      overlay.style.display = "block";
     });
   });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   initApp();
 });
+
+// DATA FORM PRESERVATION
+const userName = document.getElementById("name");
+const message = document.getElementById("msg");
+
+function keepData() {}
